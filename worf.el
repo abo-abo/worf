ca9a42b64938f43d757c6e0c41f21610bea87dba
Oleh Krehel <ohwoeowho@gmail.com>
2016-12-31 11:17:12 +0100

    worf.el (worf-todo): Increment only :recurring: tasks
    Fixes #27

--- worf.el
+++ worf.el
@@ -1205,7 +1205,7 @@ When ARG is true, add a CUSTOM_ID first."
               (when (string= keyword "DONE")
                 (save-excursion
                   (org-back-to-heading)
-                  (when (looking-at "[^0-9]*\\([0-9]+\\)")
+                  (when (looking-at "[^0-9]*\\([0-9]+\\)[^0-9]*:recurring:$")
                     (let ((idx (string-to-number (match-string 1))))
                       (replace-match (prin1-to-string (1+ idx))
                                      nil t nil 1)))))
