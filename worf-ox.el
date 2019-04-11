166913780e13e6d65bf32ff2102ec7ebbedfa334
Oleh Krehel <ohwoeowho@gmail.com>
2019-04-11 15:54:54 +0200

    worf-ox.el (make-el-links-symbols): Add

--- worf-ox.el
+++ worf-ox.el
@@ -32,4 +32,40 @@
 (setq org-html-style-default nil)
 (setq org-html-head-include-scripts nil)
 
+(setq org-export-filter-parse-tree-functions '(make-el-links-symbols))
+
+(defun make-el-links-symbols (tree back-end channel)
+  (el-filter-tree tree))
+
+(defun el-filter-tree (tree)
+  (cond ((not (consp tree))
+         tree)
+        ((eq (car tree) 'link)
+         (let ((plist (nth 1 tree)))
+           (if (equal (plist-get plist :type) "el")
+               `(verbatim
+                 (
+                  :value ,(plist-get plist :path)
+                  :begin ,(plist-get plist :begin)
+                  :end ,(plist-get plist :end)
+                  :post-blank ,(plist-get plist :post-blank)
+                  :parent ,(plist-get plist :parent)))
+             tree)))
+        ((eq (car tree) :parent)
+         tree)
+        (t
+         (setcar tree (el-filter-tree (car tree)))
+         (setcdr tree (el-filter-tree (cdr tree)))
+         tree)))
+
+(defun strip-org-object (x)
+  (if (and (list x)
+           (> (length x) 1)
+           (listp (nth 1 x)))
+      (let* ((plist (nth 1 x))
+             (p (cl-position :parent plist)))
+        (setf (nth (1+ p) plist) nil)
+        x)
+    x))
+
 (provide 'worf-ox)
