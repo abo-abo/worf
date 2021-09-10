3d950c735caa334d42cf3a1a6e4b0beb9d42bdaa
Oleh Krehel <ohwoeowho@gmail.com>
2021-09-10 19:33:36 +0200

    worf.el (worf--at-property-p): Update regex

--- worf.el
+++ worf.el
@@ -1530,7 +1530,7 @@ When ARG is 2, and the item was scheduled, make it done at that time."
 ;; ——— Predicates ——————————————————————————————————————————————————————————————
 (defun worf--at-property-p ()
   "Return t if point is at property."
-  (looking-at "^:\\(?:property\\|end\\)"))
+  (looking-at "^:\\(?:property\\|properties\\|end\\|logbook\\)"))
 
 (defun worf--special-p ()
   "Return t if point is special.
